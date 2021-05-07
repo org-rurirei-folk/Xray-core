cc1444a937581d46c2f74b27e1b9b12f029d1a2a
rurirei <72071920+rurirei@users.noreply.github.com>
2021-05-08 05:26:46 +0800

    feat: ToContext (#14)

--- core/context.go
+++ core/context.go
@@ -4,10 +4,10 @@ import (
 	"context"
 )
 
-// XrayKey is the key type of Instance in Context, exported for test.
-type XrayKey int
+// xrayKeyType is the key type of Instance in Context.
+type xrayKeyType int
 
-const xrayKey XrayKey = 1
+const xrayKey xrayKeyType = 1
 
 // FromContext returns an Instance from the given context, or nil if the context doesn't contain one.
 func FromContext(ctx context.Context) *Instance {
@@ -24,4 +24,12 @@ func MustFromContext(ctx context.Context) *Instance {
 		panic("X is not in context.")
 	}
 	return x
+}
+
+// ToContext returns ctx from the given context, or creates an Instance if the context doesn't find that.
+func ToContext(ctx context.Context, v *Instance) context.Context {
+	if FromContext(ctx) != v {
+		ctx = context.WithValue(ctx, xrayKey, v)
+	}
+	return ctx
 }
